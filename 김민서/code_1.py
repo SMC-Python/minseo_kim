a07b822fc896cf3948022187ca6e6efea299df50
ingansanangggun <107869437+ingansanangggun@users.noreply.github.com>
2022-10-24 19:42:06 +0900

    Add files via upload

---
+++ 김민서/code_1.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/hello') #: 행위 / 자원
+def hello():
+    return {'test': 'hello2'} #: 표현
+
+
+@app.post('/users') #: 행위 / 자원
+def create_user(username: str):
+    return {'username': username}
+
+
+@app.get('/users') #: 행위 / 자원
+def create_user(username: str):
+    return {'username': username}
+
+
+@app.get('/users/{username}') #: 행위 / 자원
+def create_user(username: str):
+    return {'username': username}
